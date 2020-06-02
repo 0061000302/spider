4c41b98ba021e11e0d44b184a5e45acdbf6d8f7c
熊宏杰 <1400781352@qq.com>
2020-06-02 23:27:15 +0800

    second commit

--- webserver.js
+++ webserver.js
@@ -28,11 +28,12 @@ if(system.args.length!==2){
                     console.log('FAIL to load the address');
                     return;
                 }
-                body=page.content;
-                //写入文件
-                // fs.write('./'+requestUrl+".html",body,'w');
-                fs.write("demo.html",body,'w');
-                response.write(body);
+                page.switchToFrame(0);
+                page.includeJs(jq,function () {
+                    page.evaluate(playListsCallback);
+                });
+                handlerPlayListDetail();
+                response.write('success');
                 response.close();
             });
         }else {
@@ -45,6 +46,114 @@ if(system.args.length!==2){
         console.log('Web server running on port'+port);
     }else {
         console.log('Error:Could not create web server listen on port'+port);
-        phantom.exit(1);
+        phantom.exit();
     }
+};
+
+var jq="http://cdn.bootcss.com/jquery/3.3.1/jquery.min.js";
+var playLists=[];
+
+page.onerror=function (message) {
+    if (message.indexOf('__data__')!==-1){
+        var data=message.substr(8);
+        console.log(data);
+        var jsonData=JSON.parse(data);
+        if (jsonData.dataType==='playLists'){
+            for (var i=0;i<jsonData.playLists.length;i++){
+                playLists.push(jsonData.playLists[i]);
+            }
+        }else if (jsonData.dataType==='playListDetail'){
+            var playListDetailFile='./'+jsonData.id+'.json';
+            if (!fs.exists(playListDetailFile)){
+                fs.write(playListDetailFile,data,'w');
+            }
+        }
+    }else {
+        console.log(message);
+    }
+};
+
+
+/**
+ *歌单回调
+ **/
+function playListsCallback() {
+    var items=[];
+    jQuery('#m-pl-container li').each(function () {
+        var target=jQuery(this);
+        var href='http://music.163.com/#'+target.find(
+            '.u-cover a').attr('href');
+        var item={
+            href:href,
+            id:target.find('.icon-play').data('res-id')
+        };
+        items.push(item);
+    });
+    var playLists={
+        dataType:'playLists',
+        playLists:items
+    };
+    console.error('__data__'+JSON.stringify(playLists,undefined,4));
+}
+
+/**
+ * 详情页回调
+ */
+function detailCallback() {
+    var playList={
+        dataType: 'playListDetail',
+        title:jQuery('.tit h2').text(),
+        id:jQuery('#content-operation').data('rid'),
+        cover:jQuery('.u-cover img').attr('src'),
+        desc:jQuery('#album-desc-dot').html(),
+        songs:[]
+    };
+    jQuery('.m-table tbody tr').each(function () {
+        var song={
+            id:jQuery(this).find('.hd span').data('res-id'),
+            name:jQuery(this).find('b').attr('title'),
+            duration:jQuery(this).find('.u-dur').text(),
+            singer:jQuery(this).find('.text').attr('title'),
+            albumName:jQuery(this).find('.text a').attr('title')
+        };
+        playList.songs.push(song);
+    });
+
+    console.error('__data__'+JSON.stringify(playList,undefined,4));
+}
+/**
+ *处理歌单详情页
+ **/
+function handlerPlayListDetail() {
+    setTimeout(invokeDetail,0);
+}
+
+/**
+ *执行详情页爬虫请求
+ **/
+function invokeDetail() {
+    if (playLists.length === 0) {
+        handlerPlayListDetail();
+        return;
+    }
+    var playListHref = playLists[0].href;
+    var playListDetailFile = './' + playLists[0].id + '.json';
+    playLists = playLists.slice(1);
+    if (fs.exists(playListDetailFile)) {
+        handlerPlayListDetail();
+        return;
+    }
+    page.open(playListHref, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address', page.url);
+            playLists.push(page.url);
+            handlerPlayListDetail();
+            return;
+        }
+        console.log('SUCCESS ti load the address', page.url);
+        page.includeJs(jq, function () {
+            page.evaluate(detailCallback);
+            handlerPlayListDetail();
+        });
+    });
 }
